f8f262fc2b25908b925e1de18a5b72df12919791
osaamaahmeed <iiosamaahmed1@gmail.com>
2026-01-19 23:21:14 +0200

    feat(stripe): initialize Stripe with publishable key

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:payme/core/utils/api_keys.dart';
 import 'package:payme/features/checkout/view/my_cart_view.dart';
 void main () {
+  Stripe.publishableKey = ApiKeys.publishableKey;
   runApp(const Payme());
 }
 
@@ -15,3 +18,7 @@ class Payme extends StatelessWidget {
     );
   }
 }
+
+// 1. [payment intent object] create payment intent (amount + currency [required])
+// 2. init payment sheet (payment intent client secret)
+// 3. present payment sheet
